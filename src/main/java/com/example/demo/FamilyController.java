041f916f45bd01c6928d6682ca59dd704180c88d
cantpauseitmum <feliks1997@gmail.com>
2023-08-03 18:17:02 +0200

    post

--- src/main/java/com/example/demo/FamilyController.java
+++ src/main/java/com/example/demo/FamilyController.java
@@ -1,10 +1,9 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -12,11 +11,11 @@ import java.util.UUID;
 @RestController
 @RequestMapping(value = "api/v1/family")
 public class FamilyController {
+    List<Family> familyList = new ArrayList<>();
+    List<Member> members = new ArrayList<>();
 
     @RequestMapping(value = "/getall", method = RequestMethod.GET)
     public List<Family> getAll() {
-        List<Family> familyList = new ArrayList<>();
-        List<Member> members = new ArrayList<>();
         members.add(new Member("Adam", 12, "M"));
         members.add(new Member("Ania", 24, "K"));
         members.add(new Member("Wojtek", 11, "M"));
@@ -27,13 +26,21 @@ public class FamilyController {
 
     @RequestMapping(value = "/getByName", method = RequestMethod.GET)
     public Family getByName(@RequestParam String familyName) {
-        List<Family> familyList = new ArrayList<>();
-        List<Member> members = new ArrayList<>();
         members.add(new Member("Adam", 12, "M"));
         members.add(new Member("Ania", 24, "K"));
         members.add(new Member("Wojtek", 11, "M"));
         familyList.add(new Family(UUID.randomUUID().toString(), "Kowalski", members));
         familyList.add(new Family(UUID.randomUUID().toString(), "Nowakowie", members));
         return familyList.stream().filter(family -> family.getName().equals(familyName)).findFirst().orElseThrow();
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public void createFamily(@RequestBody Family family, HttpServletResponse response) throws IOException {
+        if (family.getName() != null && !family.getMembers().isEmpty()) {
+            familyList.add(family);
+            response.sendError(HttpServletResponse.SC_OK, "Dodano do listy");
+            return;
+        }
+        response.sendError(HttpServletResponse.SC_CONFLICT, "Nazwa rodziny nie może być pusta oraz lista członków nie może być mniejsza niż 1");
     }
 }
