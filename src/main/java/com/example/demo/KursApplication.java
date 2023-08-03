a9ee41eda18639d820e6e02363c252488d2c947b
hfelicjaniak <hfelicjaniak@edu.cdv.pl>
2023-08-03 17:16:05 +0200

    Beans

--- src/main/java/com/example/demo/KursApplication.java
+++ src/main/java/com/example/demo/KursApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KursApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KursApplication.class, args);
+	}
+
+	@Bean
+	public Call call(){
+		return new Call();
 	}
 
 }
